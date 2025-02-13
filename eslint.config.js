709b41d1e155109cef0aafe07ed056dfc7f26d2d
oneoneniaoniao <76186907+oneoneniaoniao@users.noreply.github.com>
2025-02-13 20:04:49 +0900

    chore: add pre-commit of ESLint and Prettier
    chore: add pre-commit of ESLint and Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,9 +17,11 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'prettier': prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': 'error', 
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
